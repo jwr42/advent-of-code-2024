694deaa89f6b242dc6209eb8afc709203bb94c96
Jonathan Roberts <jonathan.w.roberts@pm.me>
2024-12-01 18:30:44 +0000

    day 1 solutions

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import numpy as np
 
-file = np.readtxt("day1-puzzle1-input.txt")
+data = np.loadtxt("day1-puzzle1-input.txt")
 
-print(file)
+# puzzle 1
+list1 = np.sort(data[:, 0])
+list2 = np.sort(data[:, 1])
+sorted = np.column_stack((list1, list2))
+print(np.sum(np.abs(np.diff(sorted))))
+
+# puzzle 2
+print(np.sum([i * np.sum(np.where(list2 == i, 1, 0)) for i in list1]))
