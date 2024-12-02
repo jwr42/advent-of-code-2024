a04b2a186dd798c80bf548f985ca384fae45f674
Jonathan Roberts <jonathan.w.roberts@pm.me>
2024-12-02 16:42:05 +0000

    update .gitignore

--- day1-solution.py
+++ day1-solution.py
@@ -1,14 +1,3 @@
-# # initial attempt at day 1
-# import numpy as np
-# data = np.loadtxt("day1-input.txt")
-# # puzzle 1
-# list1 = np.sort(data[:, 0])
-# list2 = np.sort(data[:, 1])
-# sorted = np.column_stack((list1, list2))
-# print(np.sum(np.abs(np.diff(sorted))))
-# # puzzle 2
-# print(np.sum([i * np.sum(np.where(list2 == i, 1, 0)) for i in list1]))
-
 # day 1 solution
 
 lines = open("day1-input.txt").readlines()
@@ -22,5 +11,3 @@ print(f"Puzzle 1 solution: {sum(diff):,}")
 
 similarity = [int(i) * int(list2.count(i)) for i in list1]
 print(f"Puzzle 2 solution: {sum(similarity):,}")
-
-# day 2 solution
